0d496b214d454e363d6c66cd7d8147e2f2c4d7de
Darsh Modi <darshmodi@microsoft.com>
2022-09-20 19:05:28 +0530

    Navigation Between Screens added

--- App.js
+++ App.js
@@ -1,6 +1,32 @@
-import Home from './screens/Home';
+// import Home from './screens/Home';
 
-export default function App() {
+// export default function App() {
 
-  return <Home />;
-}
+//   return <Home />;
+// }
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Navbar from "./components/Navbar";
+import HomeScreen from "./screens/Home"
+import UploadMedicineDetailsScreen from "./screens/UploadMedicineDetails"
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={<Navbar />}
+        />
+        <Stack.Screen name="UploadMedicineDetails" component={UploadMedicineDetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MyStack;
